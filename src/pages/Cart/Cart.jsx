5a91867494c058120b58244d0168c7e9399633fe
KuzAnton8335 <ant2783@gmail.com>
2025-05-14 15:03:25 +0300

    добавил страницу корзины

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,81 @@
+import { SearchCategories } from '../../components/SearchCategories/SearchCategories';
+import { Footer } from '../../layouts/Footer/Footer.jsx';
+import { Header } from '../../layouts/Header/Header.jsx';
 import './Cart.scss';
 
 const Cart = () => {
-	return <div>Cart</div>;
+	return (
+		<div className="cart">
+			<Header />
+			<div className="container">
+				<div className="cart__top">
+					<SearchCategories />
+				</div>
+				<div className="cart__wrapper">
+					<div className="cart__content">
+						<div className="cart__content-body">
+							<button className="cart__content-body-btn">
+								<svg
+									width="25"
+									height="25"
+									viewBox="0 0 25 25"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M25 22.6165C25 23.933 23.933 25 22.6165 25H2.3835C1.067 25 0 23.933 0 22.6165V2.3835C0 1.067 1.067 0 2.3835 0H22.6165C23.933 0 25 1.067 25 2.3835V22.6165Z"
+										fill="#E9AB74"
+									/>
+									<path
+										opacity="0.2"
+										d="M18.5144 7.671L7.6709 18.5145L14.1564 25H22.6164C23.9329 25 24.9999 23.933 24.9999 22.6165V14.1565L18.5144 7.671Z"
+										fill="black"
+									/>
+									<path
+										d="M18.5146 16.241C18.8151 16.5415 18.8151 17.029 18.5146 17.3295L17.3291 18.515C17.0286 18.8155 16.5411 18.8155 16.2406 18.515L6.48563 8.759C6.18513 8.4585 6.18513 7.971 6.48563 7.6705L7.67113 6.485C7.97163 6.1845 8.45913 6.1845 8.75963 6.485L18.5146 16.241Z"
+										fill="white"
+									/>
+									<path
+										d="M16.2411 6.48551C16.5416 6.18501 17.0291 6.18501 17.3296 6.48551L18.5151 7.67101C18.8156 7.97151 18.8156 8.45901 18.5151 8.75951L8.75914 18.5145C8.45864 18.815 7.97114 18.815 7.67064 18.5145L6.48514 17.329C6.18464 17.0285 6.18464 16.541 6.48514 16.2405L16.2411 6.48551Z"
+										fill="white"
+									/>
+								</svg>
+							</button>
+							<div className="cart__content-images">
+								<img
+									src="/src/assets/images/card-img-5.jpg"
+									width="225"
+									height="243"
+									alt="img"
+									className="cart__content-image"
+								/>
+								<p className="cart__content-id">ID товара: 44 </p>
+							</div>
+							<div className="cart__content-info">
+								<h3 className="cart__content-title">
+									Электродвигатель 5АИ
+								</h3>
+								<p className="cart__content-quantity">Количество: 2шт</p>
+								<p className="cart__content-price">
+									Стоимость: 10000 руб.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="cart__right">
+						<div className="cart__total">
+							<h4 className="cart__total-title">Итого</h4>
+							<div className="cart__total-info">
+								<span>Итоговая сумма: 20000 руб.</span>
+							</div>
+							<button className="cart__total-btn">Оформить Заказ</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default Cart;
